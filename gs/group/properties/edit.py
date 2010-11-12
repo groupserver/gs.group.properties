a570d080a4e15af1d3e159bfcba33fe54a739b41
Alice Murphy <alice@onlinegroups.net>
2010-11-12 22:55:10 +0100

    Fixes to get basic page up. Hgignore.

--- gs/group/properties/edit.py
+++ gs/group/properties/edit.py
@@ -10,6 +10,16 @@ class EditPropertiesForm(GroupForm):
     
     def __init__(self, context, request):
         GroupForm.__init__(self, context, request)
-        self.form_fields = form.Fields(IGroupProperties, render_context=True)
-        self.label = u'Edit Group Properties'
+        self.form_fields = form.Fields(IGroupProperties)
+        self.label = u'Change Group Properties'
         
+    @form.action(label=u'Change', failure='handle_change_action_failure')
+    def handle_change(self, action, data):
+        self.status = u'Something changed!'
+
+    def handle_change_action_failure(self, action, data, errors):
+        if len(errors) == 1:
+            self.status = u'<p>There is an error:</p>'
+        else:
+            self.status = u'<p>There are errors:</p>'
+            
