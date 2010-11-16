d8faf799da085594e58fa648a59f86f98aafd30b
Alice Murphy <alice@onlinegroups.net>
2010-11-16 18:04:15 +0100

    Wording

--- gs/group/properties/interfaces.py
+++ gs/group/properties/interfaces.py
@@ -6,16 +6,16 @@ class IGroupProperties(Interface):
     title = TextLine(title=u'Group Name',
         description=u'The name of the group',
         required=True)
-    description = TextLine(title=u'Description',
+    description = TextLine(title=u'Short Description',
         description=u'Who and what the group is for',
         required=True)
-    short_name = TextLine(title=u'Subject Prefix',
+    short_name = TextLine(title=u'Subject Line Prefix',
         description=u'Shown in the subject line of all group '\
           u'emails',
         required=True)
-    mshipCriterion = TextLine(title=u'Membership Criterion (used '\
-          u'only when the group\'s privacy setting is Private or '\
-          u'Secret)',
+    mshipCriterion = TextLine(title=u'Membership Criteria (displayed '\
+          u'only when the group\'s privacy setting is "private" or '\
+          u'"secret")',
         description=u'Who is eligible to join the group',
         required=True)
     
