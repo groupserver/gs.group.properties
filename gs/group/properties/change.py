a7f891d20194ca9ea319f09ae2f8738dfefbc020
Alice Murphy <alice@onlinegroups.net>
2010-11-16 17:09:18 +0100

    Render context. Disable mship criterion field when group is public.
    --HG--
    rename : gs/group/properties/browser/templates/editproperties.pt => gs/group/properties/browser/templates/changeproperties.pt
    rename : gs/group/properties/edit.py => gs/group/properties/change.py

---
+++ gs/group/properties/change.py
@@ -0,0 +1,57 @@
+# coding=utf-8
+from zope.formlib import form
+from zope.app.form.browser import TextWidget
+from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
+from gs.content.form.utils import enforce_schema
+from gs.group.privacy.visibility import GroupVisibility
+from gs.group.base.form import GroupForm
+from interfaces import IGroupProperties
+
+def disabled_textwidget(field, request):
+    retval = TextWidget(field, request)
+    retval.cssClass = "disabled"
+    return retval
+
+class ChangePropertiesForm(GroupForm):
+    pageTemplateFileName = 'browser/templates/changeproperties.pt'
+    template = ZopeTwoPageTemplateFile(pageTemplateFileName)
+    
+    def __init__(self, context, request):
+        GroupForm.__init__(self, context, request)
+        enforce_schema(self.context, IGroupProperties)
+        self.groupVisibility = GroupVisibility(self.groupInfo)
+        self.__form_fields = None
+        self.label = u'Change Group Properties'
+        
+    @property
+    def form_fields(self):
+        if self.__form_fields == None:
+            form_fields = \
+              form.Fields(IGroupProperties, render_context=True)
+            if self.groupVisibility.isPublic:
+                form_fields['mshipCriterion'].custom_widget = \
+                  disabled_textwidget
+            self.__form_fields = form_fields
+        return self.__form_fields
+    
+    def setUpWidgets(self, ignore_request=False):
+        self.widgets = form.setUpWidgets(
+            self.form_fields, self.prefix, self.context,
+            self.request, form=self,
+            ignore_request=ignore_request)
+        if self.groupVisibility.isPublic:
+            self.widgets['mshipCriterion'].required = False
+    
+    @form.action(label=u'Change', failure='handle_change_action_failure')
+    def handle_change(self, action, data):
+        form.applyChanges(self.context, self.form_fields, data)
+        self.status = u'Changed the properties of '\
+          u'<a href="%s">%s</a>.' %\
+           (self.groupInfo.relative_url(), self.groupInfo.name)
+
+    def handle_change_action_failure(self, action, data, errors):
+        if len(errors) == 1:
+            self.status = u'<p>There is an error:</p>'
+        else:
+            self.status = u'<p>There are errors:</p>'
+            
--- gs/group/properties/interfaces.py
+++ gs/group/properties/interfaces.py
@@ -3,20 +3,19 @@ from zope.interface import Interface
 from zope.schema import TextLine
 
 class IGroupProperties(Interface):
-    groupName = TextLine(title=u'Group Name',
-        description=u'The name of the group.',
+    title = TextLine(title=u'Group Name',
+        description=u'The name of the group',
         required=True)
     description = TextLine(title=u'Description',
-        description=u'A description of the group.',
+        description=u'Who and what the group is for',
         required=True)
-    subjectPrefix = TextLine(title=u'Subject Prefix',
-        description=u'A short identifier to be prepended '\
-          u'to the subject line of all group emails, '\
-          u'within square brackets',
+    short_name = TextLine(title=u'Subject Prefix',
+        description=u'Shown in the subject line of all group '\
+          u'emails',
         required=True)
-    mshipCriterion = TextLine(title=u'Membership Criterion',
-        description=u'A short identifier to be prepended '\
-          u'to the subject line of all group emails, '\
-          u'within square brackets',
-        required=False)
+    mshipCriterion = TextLine(title=u'Membership Criterion (used '\
+          u'only when the group\'s privacy setting is Private or '\
+          u'Secret)',
+        description=u'Who is eligible to join the group',
+        required=True)
     
