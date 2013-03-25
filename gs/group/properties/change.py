bbf33c68dd11d907cc96be98ed38c10f86e3c614
Michael JasonSmith <mpj17@onlinegroups.net>
2013-03-25 16:03:49 +1100

    Code cleanup helped by Ninja-IDE

--- gs/group/properties/change.py
+++ gs/group/properties/change.py
@@ -1,35 +1,31 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
+from zope.cachedescriptors.property import Lazy
 from zope.formlib import form
-from zope.app.form.browser import TextWidget
 from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
-from gs.content.form.disabledtextwidget import disabled_text_widget
+from gs.content.form import disabled_text_widget
 from gs.content.form.utils import enforce_schema
 from gs.group.privacy.visibility import GroupVisibility
 from gs.group.base.form import GroupForm
 from interfaces import IGroupProperties
 
+
 class ChangePropertiesForm(GroupForm):
     pageTemplateFileName = 'browser/templates/changeproperties.pt'
     template = ZopeTwoPageTemplateFile(pageTemplateFileName)
-    
+
     def __init__(self, context, request):
         GroupForm.__init__(self, context, request)
         enforce_schema(self.context, IGroupProperties)
         self.groupVisibility = GroupVisibility(self.groupInfo)
-        self.__form_fields = None
         self.label = u'Change Group Properties'
-        
-    @property
+
+    @Lazy
     def form_fields(self):
-        if self.__form_fields == None:
-            form_fields = \
-              form.Fields(IGroupProperties, render_context=True)
-            if self.groupVisibility.isPublic:
-                form_fields['mshipCriterion'].custom_widget = \
-                  disabled_text_widget
-            self.__form_fields = form_fields
-        return self.__form_fields
-    
+        retval = form.Fields(IGroupProperties, render_context=True)
+        if self.groupVisibility.isPublic:
+            retval['mshipCriterion'].custom_widget = disabled_text_widget
+        return retval
+
     def setUpWidgets(self, ignore_request=False):
         self.widgets = form.setUpWidgets(
             self.form_fields, self.prefix, self.context,
@@ -37,7 +33,7 @@ class ChangePropertiesForm(GroupForm):
             ignore_request=ignore_request)
         if self.groupVisibility.isPublic:
             self.widgets['mshipCriterion'].required = False
-    
+
     @form.action(label=u'Change', failure='handle_change_action_failure')
     def handle_change(self, action, data):
         form.applyChanges(self.context, self.form_fields, data)
@@ -51,4 +47,3 @@ class ChangePropertiesForm(GroupForm):
             self.status = u'<p>There is an error:</p>'
         else:
             self.status = u'<p>There are errors:</p>'
-            
--- gs/group/properties/interfaces.py
+++ gs/group/properties/interfaces.py
@@ -3,25 +3,29 @@ from zope.interface import Interface
 from zope.schema import TextLine
 from zope.viewlet.interfaces import IViewletManager
 
+
 class IGroupProperties(Interface):
     title = TextLine(title=u'Group Name',
         description=u'The name of the group',
         required=True)
+
     description = TextLine(title=u'Short Description',
-        description=u'Who and what the online group is for. '\
-          u'Shown on the Groups page and the Group Home Page.',
+        description=u'Who and what the online group is for. '
+            u'Shown on the Groups page and the Group Home Page.',
         required=True)
+
     short_name = TextLine(title=u'Subject Line Prefix',
-        description=u'Shown in the subject line of posts '\
-          u'delivered via email.',
+        description=u'Shown in the subject line of posts '
+            u'delivered via email.',
         required=True)
-    mshipCriterion = TextLine(title=u'Membership Criterion — '\
-          u'displayed only when the group\'s privacy setting '\
-          u'is "private" or "secret".',
-        description=u'Who is eligible to join the group in '\
-          u'the future?',
+
+    mshipCriterion = TextLine(title=u'Membership Criterion — '
+              u'displayed only when the group\'s privacy setting '
+              u'is "private" or "secret".',
+        description=u'Who is eligible to join the group in '
+              u'the future?',
         required=True)
-    
+
+
 class IGroupAdminPropertiesLinks(IViewletManager):
     '''A viewlet manager for the Properties tabs used by the group admin'''
-
