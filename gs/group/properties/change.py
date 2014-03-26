69a06d458223faa3ee9e65d4bcbd235d502d3432
Michael JasonSmith <mpj17@onlinegroups.net>
2014-03-26 12:05:29 -0700

    Switching to Unicode literals and absolute_import

--- gs/group/properties/change.py
+++ gs/group/properties/change.py
@@ -1,12 +1,26 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2013, 2014 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+from __future__ import absolute_import, unicode_literals
 from zope.cachedescriptors.property import Lazy
 from zope.formlib import form
 from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
 from gs.content.form import disabled_text_widget
 from gs.content.form.utils import enforce_schema
-from gs.group.privacy.visibility import GroupVisibility
-from gs.group.base.form import GroupForm
-from interfaces import IGroupProperties
+from gs.group.privacy import GroupVisibility
+from gs.group.base import GroupForm
+from .interfaces import IGroupProperties
 
 
 class ChangePropertiesForm(GroupForm):
@@ -17,7 +31,7 @@ class ChangePropertiesForm(GroupForm):
         GroupForm.__init__(self, context, request)
         enforce_schema(self.context, IGroupProperties)
         self.groupVisibility = GroupVisibility(self.groupInfo)
-        self.label = u'Change Group Properties'
+        self.label = 'Change Group Properties'
 
     @Lazy
     def form_fields(self):
@@ -34,16 +48,16 @@ class ChangePropertiesForm(GroupForm):
         if self.groupVisibility.isPublic:
             self.widgets['mshipCriterion'].required = False
 
-    @form.action(label=u'Change', failure='handle_change_action_failure')
+    @form.action(label='Change', failure='handle_change_action_failure')
     def handle_change(self, action, data):
         form.applyChanges(self.context, self.form_fields, data)
         # TODO: https://projects.iopen.net/groupserver/ticket/640
-        self.status = u'Changed the properties of '\
-          u'<a href="%s">%s</a>.' %\
+        self.status = 'Changed the properties of '\
+          '<a href="%s">%s</a>.' %\
            (self.groupInfo.relative_url(), self.groupInfo.name)
 
     def handle_change_action_failure(self, action, data, errors):
         if len(errors) == 1:
-            self.status = u'<p>There is an error:</p>'
+            self.status = '<p>There is an error:</p>'
         else:
-            self.status = u'<p>There are errors:</p>'
+            self.status = '<p>There are errors:</p>'
